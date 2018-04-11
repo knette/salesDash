6f468ac5c1c1b3c49a8f06f47e702dd86c93eb88
kristina nette <kristina.nette@gmail.com>
2018-04-10 17:00:08 -0700

    formatting for table

--- client/src/views/Sales.jsx
+++ client/src/views/Sales.jsx
@@ -1,35 +1,57 @@
 import React from 'react'
+import httpClient from '../httpClient.js'
+import { Link } from 'react-router-dom';
 
-const Sales = (props) => {
-	return (
+class Sales extends React.Component { 
+	state= {
+		sales: []
+	}
+	componentDidMount() {
+		httpClient.getAllSales().then((serverResponse) => {
+			this.setState({
+				sales: serverResponse.data
+			})
+		})
+	}
+
+	render () {
+		const { sales } = this.state
+		return (
 		<div className='Sales'>
-			<h1>Your Sales</h1>
+			<h1>Sales</h1>
 			<table>
   				<thead>
 					<tr>
-					<th>Name</th>
-					<th>Age</th>
-					<th>Height</th>
-					<th>Location</th>
+					<th>Company</th>
+					<th>Sale Price</th>
+					<th>Commission %</th>
+					<th>Commission $</th>
+					<th>Invoice Date</th>
+					<th>Return?</th>
+					<th>Delete</th>
+					<th>Edit</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>Stephen Curry</td>
-						<td>27</td>
-						<td>1,91</td>
-						<td>Akron, OH</td>
-					</tr>
-					<tr>
-						<td>Klay Thompson</td>
-						<td>25</td>
-						<td>2,01</td>
-						<td>Los Angeles, CA</td>
-					</tr>
+					{sales.map((s) => {
+                  return (
+                  <tr key={s._id}>
+                    <td>{s.company}</td> 
+                    <td>${s.price}</td>
+                    <td>{(s.commission/100).toFixed(2)}</td>
+					<td>${(s.commission * s.price).toFixed(2)}</td>
+                    <td>{(s.invoiceDate)}</td>
+                    <td>{s.refund}</td>
+					<th>Delete</th>
+					<th>Edit</th>
+                    </tr>
+                  )}) 
+                }
 				</tbody>
 			</table>
 		</div>
-	)
+		)
+	}
 }
 
 export default Sales
